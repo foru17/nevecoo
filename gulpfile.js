91d65efc77df15a2c17eae800d9f5b136e3d3072
luolei <meleilei@gmail.com>
2015-02-11 18:45:42 +0800

    切换到本地模板调试

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,19 @@ var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 var jshint = require('gulp-jshint');
+
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var pngquant = require('imagemin-pngquant');
+
+
 var autoprefixer = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+// 图像处理
+var imagemin = require('gulp-imagemin');
+var spritesmith = require('gulp.spritesmith');
+var imageResize = require('gulp-image-resize');
 
 // 设置相关路径
 var paths = {
@@ -34,17 +43,39 @@ gulp.task('clean', function(cb) {
 // Sass 处理
 gulp.task('sass', function() {
     gulp.src(paths.sass)
+        .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(gulp.dest('css'))
         .pipe(concat('style.css'))
         .pipe(gulp.dest(paths.css))
         .pipe(minifycss())
+        .pipe(sourcemaps.write({sourceRoot: '/css/sass'}))
         .pipe(rename('style.min.css'))
         .pipe(gulp.dest('assets/css'));
 });
 
+// 图片精灵处理
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('img/sprite/*.png').pipe(spritesmith({
+    imgName: 'sprite@2x.png',
+    cssName: '_sprite.scss',
+     algorithm: 'alt-diagonal'
+  })
+  );
+   spriteData.img.pipe(gulp.dest('./img/')); // 输出合成图片
+   spriteData.css.pipe(gulp.dest('./css/sass/')); // 输出的CSS
+  // spriteData.pipe(gulp.dest('path/to/output/'));
+});
 
 
+gulp.task('half', function () {
+  gulp.src('./img/*.png')
+    .pipe(imageResize({
+      width : 100,
+      height : 100
+    }))
+    .pipe(gulp.dest('./img/1x/'));
+});
 
 // JS检查
 gulp.task('lint', function() {
@@ -62,6 +93,19 @@ gulp.task('scripts', ['clean'], function() {
         .pipe(concat('all.min.js'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('assets/js'));
+});
+
+
+
+// 处理图像
+gulp.task('image', function () {
+    return gulp.src(paths.img)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('assets/images'));
 });
 
 
--- js/base.js
+++ js/base.js
@@ -18,28 +18,160 @@ $.fn.extend({
 
 
 
+var General = {
+    init: function() {
+        General.scrollToPos();
+        General.checkKey();
+    },
+
+    //模拟鼠标上下滚动
+    checkKey: function(e) {
+        e = e || window.event;
+        if (e.keyCode == '74') {
+            console.log('按下键盘');
+            $('html,body').stop();
+            $('html,body').animate({
+                scrollTop: $(window).stop().scrollTop() + 200
+            }, 'fast')
+        } else if (e.keyCode == '75') {
+            $('html,body').stop();
+            $('html,body').animate({
+                scrollTop: $(window).stop().scrollTop() - 200
+            }, 'fast')
+        }
+    },
+
+
+    //平滑滚动到顶部
+    scrollToPos: function(position) {
+        var STR_TO_TOP = '我要飞到最高',
+            coverHeight = position || $(window).height(); //获得图片高度
+        var button = $('<a href="#" id="to-top" title="' + STR_TO_TOP + '"> <div class="to-top-wrap"></div></a>').appendTo('body');
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > $(window).height()) {
+                button.fadeIn(500);
+            } else {
+                button.fadeOut(500);
+            }
+        });
+
+        button.click(function(e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: coverHeight - 50
+            }, 1000, function() {
+                window.location.hash = '#';
+            });
+            console.log('我跳');
+        })
+    },
+    //处理屏幕变化时
+    resize: function() {
+        visualContainerWidh = $(window).width();
+        // contentWidth=$('.post-content').width();
+        // $('.single-post-inner img').each(function() {
+        //     var _img = $(this);
+        //     checkImg(_img);
+        // })
+    }
+
+
+
+}
+
 
 $(document).ready(function() {
     console.log('加载完毕');
+    General.init();
 
-    $('.post_in_list').each(function() {
+
+    $('.single-post-inner p:has(img)').each(function() {
+
+        var _this = $(this);
+        _this.addClass('with-img');
+        if (_this.isOnScreenVisible() == true) {
+            _this.addClass('with-img').addClass('already-visible');
+        }
+
+    })
+
+
+
+
+    $('.post-in-list').each(function() {
         var _this = $(this);
         if (_this.isOnScreenVisible() == true) {
             console.log('可见');
             _this.addClass('already-visible')
         }
     })
+    $('.share h4').on('click', function() {
+        $('.share-icons').css('display', 'block').addClass('fadeInUpBig animated')
+        $(this).fadeOut(500);
+
+        setTimeout(function() {
+            $('html,body').animate({
+                scrollTop: $('#share-icons').offset().top - $(window).height() / 2
+            }, 1000, function() {
+                // window.location.hash = '#';
+            });
+        }, 1000)
+
+
+    })
+
+
+
+    // $('.share h4').hover(function() {
+    //     console.log('点击');
+    //     $('.share-icons').css('display','block').addClass('fadeInUpBig animated')
+    //     $(this).fadeOut(500);
+    // }, function() {
+    //     // $('.share-icons').fadeOut(500)
+    // })
+
+    $('#header').click(function() {
+        console.log('下滑');
+        $('html,body').animate({
+            scrollTop: $(window).height() - 20
+        }, 1000, function() {
+            // window.location.hash = '#';
+        });
+        return false;
+    })
 
 
     $(window).scroll(function() {
-        $('.post_in_list').each(function() {
+
+        if ($(window).scrollTop() > 50) {
+            // $('.nav-header').removeClass('hide').addClass('fadeInDownBig animated')
+        }
+
+        if ($('.share-icons').isOnScreenVisible() == false) {
+            $('.share-icons').removeClass('fadeInUpBig animated').css('display', 'none')
+            $('.share h4').css('display', 'block');
+        }
+
+
+        $('.post-in-list').each(function() {
             var _this = $(this);
             if (_this.isOnScreenVisible() == true && _this.hasClass('already-visible') != true) {
-                // console.log($(this));
                 _this.addClass('fadeInUpBig animated')
             }
         })
-    })
 
+        $('.single-post-inner p:has(img)').each(function() {
+            var _this = $(this);
+            if (_this.isOnScreenVisible() == true && _this.hasClass('already-visible') != true) {
+                setTimeout(function() {
+                    _this.addClass('with-img');
+                }, 200)
+
+            }
+        })
+
+
+
+    })
 
 })
